3cc7b888f6a7b7f76636ab1c1244936722f6762b
min4034415 <min4034415@gmail.com>
2024-06-24 23:03:03 +0900

    holy crap

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Ouimin Lee on 6/24/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+    }
+}
+
+#Preview {
+    CircleImage()
+}
--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,12 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("Turtle Rock")
-                .font(.title)
+                    .font(.title)
                 .foregroundStyle(.green)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
         }
-        .padding()
+        .padding() 
     }
 }
 

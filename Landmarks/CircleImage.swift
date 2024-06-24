2a818cfef05668f3f66b18fc5e14c314ea686c0d
min4034415 <min4034415@gmail.com>
2024-06-24 23:43:13 +0900

    Everywhere I look I see you

--- Landmarks/CircleImage.swift
+++ Landmarks/CircleImage.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
     }
 }
 

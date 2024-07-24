9abfd3f186589c9e71fc7d02bc316963bff8067c
min4034415 <min4034415@gmail.com>
2024-07-24 11:17:02 +0900

    Dusty by ed sheeran

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter{
+            landmark in (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
     var body: some View {
         //        List {
         //                LandmarkRow(landmark: landmarks[0])
         //                LandmarkRow(landmark: landmarks[1])
         //        }
         NavigationSplitView{
-            List(landmarks) {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+            
+            ForEach(filteredLandmarks) {
                 landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
@@ -22,6 +34,8 @@ struct LandmarkList: View {
                     LandmarkRow(landmark: landmark)
                 }
             }
+            }
+            .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
         } detail: {
             Text("Select a Landmark")

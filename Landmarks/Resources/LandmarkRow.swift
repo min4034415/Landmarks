c6e627b3b75aa50efdbb53ccceba579934aac89c
min4034415 <min4034415@gmail.com>
2024-08-05 11:07:23 +0900

    not much time left to be honest

--- Landmarks/Resources/LandmarkRow.swift
+++ Landmarks/Resources/LandmarkRow.swift
@@ -27,7 +27,8 @@ struct LandmarkRow: View {
 
 
 #Preview("Salmon") {
-    Group {
+    let landmarks = ModelData().landmarks
+    return Group {
         LandmarkRow(landmark: landmarks[0])
         LandmarkRow(landmark: landmarks[1])
     }
--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @Environment(ModelData.self) var modelData
     @State private var showFavoritesOnly = false
     
     var filteredLandmarks: [Landmark] {
-        landmarks.filter{
+        modelData.landmarks.filter{
             landmark in (!showFavoritesOnly || landmark.isFavorite)
         }
     }
@@ -45,4 +46,5 @@ struct LandmarkList: View {
 
 #Preview {
     LandmarkList()
+        .environment(ModelData())
 }

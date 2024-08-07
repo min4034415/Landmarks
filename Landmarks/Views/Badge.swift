3e679dc2b3eb45a6b45872d86f43ca9d3fa123e5
min4034415 <min4034415@gmail.com>
2024-08-07 23:37:56 +0900

    Yeosu Bam BBada

---
+++ Landmarks/Views/Badge.swift
@@ -0,0 +1,18 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Ouimin Lee on 8/7/24.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        BadgeBackground()
+    }
+}
+
+#Preview {
+    Badge()
+}

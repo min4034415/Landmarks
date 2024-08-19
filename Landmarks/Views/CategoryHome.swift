ddb82c91455f8d29d7315c8855767c143f3dc3b8
min4034415 <min4034415@gmail.com>
2024-08-19 16:29:06 +0900

    pretentious

--- Landmarks/Views/CategoryHome.swift
+++ Landmarks/Views/CategoryHome.swift
@@ -8,10 +8,23 @@
 import SwiftUI
 
 struct CategoryHome: View {
+    @Environment(ModelData.self) var modelData
+    
     var body: some View {
         NavigationSplitView {
-            Text("Hello, World!")
-                .navigationTitle("Featured")
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
         } detail: {
             Text("Select a Landmark")
         }
@@ -20,4 +33,5 @@ struct CategoryHome: View {
 
 #Preview {
     CategoryHome()
+        .environment(ModelData())
 }
---
+++ Landmarks/Views/CategoryRow.swift
@@ -0,0 +1,36 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Ouimin Lee on 8/19/24.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading){
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0){
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+#Preview {
+    let landmarks = ModelData().landmarks
+    return CategoryRow(
+        categoryName: landmarks[0].category.rawValue, items: Array(landmarks.prefix(4)))
+}

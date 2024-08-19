450e1e92914133bd945336728c2e66206c573c03
min4034415 <min4034415@gmail.com>
2024-08-19 15:40:10 +0900

    Story of Night fall

---
+++ Landmarks/Views/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Ouimin Lee on 8/19/24.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationSplitView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+}
